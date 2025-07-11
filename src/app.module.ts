8ee13121c04a611c20cd018be30d5642a0e0cb5f
Dandy Candra <dandycandrapratama@gmail.com>
2025-07-11 10:23:57 +0700

    declare import new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from 'prisma/prisma.module';
+import { AuthModule } from 'auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { WalletModule } from './wallet/wallet.module';
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    WalletModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
