0c7d1c287f41c898fe69f9dd142f53efbdb170b4
Dandy Candra <dandycandrapratama@gmail.com>
2025-07-16 23:17:37 +0700

    declare session module

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import * as session from 'express-session';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './response.interceptor';
 import { HttpExceptionFilter } from './http-exception.filter';
@@ -10,6 +11,13 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET!,
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
